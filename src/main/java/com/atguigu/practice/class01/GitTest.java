ea56a3410993b7798e6d30534ca82e8b6fca4779
qylGit <1239174190@qq.com`>
2022-04-09 00:29:56 +0800

    测试Git的使用,第san次 commit
    Signed-off-by: qylGit <1239174190@qq.com`>

--- src/main/java/com/atguigu/practice/class01/GitTest.java
+++ src/main/java/com/atguigu/practice/class01/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("first commit"+"I love China");
         System.out.println("second commit"+"I love China");
+        System.out.println("third commit"+"I love China");
     }
 }
