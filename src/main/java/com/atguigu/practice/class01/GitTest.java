8705f15619c1734298fd47c9ea06ed0084bfe79a
qylGit <1239174190@qq.com`>
2022-04-09 00:28:41 +0800

    测试Git的使用,第二次 commit
    Signed-off-by: qylGit <1239174190@qq.com`>

--- src/main/java/com/atguigu/practice/class01/GitTest.java
+++ src/main/java/com/atguigu/practice/class01/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.practice.class01;
 public class GitTest {
     public static void main(String[] args){
         System.out.println("first commit"+"I love China");
+        System.out.println("second commit"+"I love China");
     }
 }
