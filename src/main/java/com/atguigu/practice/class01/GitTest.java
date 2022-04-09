bbb578154915315677d947b9013221f9a6903e36
qylGit <1239174190@qq.com`>
2022-04-09 23:32:47 +0800

    测试Git的使用,push到远程库测试 commit by master
    Signed-off-by: qylGit <1239174190@qq.com`>

--- src/main/java/com/atguigu/practice/class01/GitTest.java
+++ src/main/java/com/atguigu/practice/class01/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("fourth commit"+"I love China");
         System.out.println("sixth commit"+"I love China by master");
         System.out.println("fifth commit"+"I love China by hit-fix");
+        System.out.println("push_test commit"+"I love China");
     }
 }
